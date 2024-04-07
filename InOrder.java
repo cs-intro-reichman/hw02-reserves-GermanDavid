f7589f01ddde198d07ff9109428f121714fdc650
GermanDavid <david.german@post.idc.ac.il>
2024-04-07 10:21:39 +0300

    Fixed the integer bug in Divisors and added functionallity to InOrder

--- InOrder.java
+++ InOrder.java
@@ -5,5 +5,15 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
+		int prev = -1;
+		while (true) {
+			int n = (int) (Math.random() * 10);
+			if (n < prev) {
+				break;
+			}
+			System.out.println(n);
+			prev = n;
+		}
+		
 	}
 }
